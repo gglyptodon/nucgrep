362c734f18f4cba6cb54f08334422413bcc9ef71
gglyptodon <janina.mass@gmail.com>
2022-03-30 14:34:00 +0200

    add reverse_complement

--- nucgrep/src/lib.rs
+++ nucgrep/src/lib.rs
@@ -1,6 +1,12 @@
+extern crate core;
+
 use clap::{Arg, Command};
+use colored::{ColoredString, Colorize};
+use seq_io::fasta::{Reader, Record};
 use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::fs::File;
+use std::io;
 use std::io::BufRead;
 
 pub type NucGrepResult<T> = Result<T, Box<dyn Error>>;
@@ -11,17 +17,57 @@ pub struct Config {
     needle: String, // string to be found in file
     file: String,   // "-" is STDIN
     reverse_complement: bool,
+    only_reverse_complement: bool,
+    allow_non_matching: usize,
+    ignore_case: bool,
+    headers_only: bool,
+}
+
+pub fn search(buff: impl BufRead) -> NucGrepResult<()> {
+    Ok(())
+}
+
+pub fn search_fasta_stdin(needle: &String, headers_only: bool) -> NucGrepResult<()> {
+    let mut reader = Reader::new(io::stdin());
+    while let Some(result) = reader.next() {
+        let tmp = result?.clone();
+        let fullseq = tmp.seq().iter().map(|&x| x as char).collect::<String>();
+        if fullseq.contains(&*needle.clone()) {
+            println!(
+                ">{}{}",
+                green(tmp.id().expect("There was a problem with the sequence ID")),
+                if !headers_only {
+                    format!("\n{}", fullseq)
+                } else {
+                    "".to_string()
+                }
+            );
+        } else {
+        }
+    }
+    Ok(())
+}
+
+pub fn green(s: &str) -> ColoredString {
+    format!("{}", s).green().bold()
 }
 
 pub fn run(config: Config) -> NucGrepResult<()> {
-    println!("{:#?}", config);
-    let mut reader = open(config.file)?;
-    let mut buff = String::new();
-    while let Ok(bytes_read) = reader.read_line(&mut buff) {
-        println!("{} {}", bytes_read, buff);
-        buff.clear();
-        if bytes_read == 0 {
-            break;
+    //println!("{:#?}", config);
+    let search_pattern = match config.reverse_complement {
+        true => reverse_complement(&config.needle, None)?, //todo rna/dna
+        _ => config.needle,
+    };
+    if config.file == "-" {
+        search_fasta_stdin(&search_pattern, config.headers_only)?; //todo refactor
+    } else {
+        let reader = open(&config.file);
+        match reader {
+            Err(e) => {
+                eprintln!("{}: {}", config.file, e);
+                std::process::exit(1)
+            }
+            Ok(file) => search(file)?,
         }
     }
     Ok(())
@@ -29,23 +75,25 @@ pub fn run(config: Config) -> NucGrepResult<()> {
 
 pub fn parse_args() -> NucGrepResult<Config> {
     let matches = Command::new("nucgrep")
-        .version("0.1.0")
+        .version("0.0.1")
         .about(
             "Find sequences in sequences.\n\
-    By default, look for PATTERN in each line of FILE. If no file is specified, use STDIN.\n",
+    By default, look for PATTERN in each line of FILE. If no file is specified, use STDIN.\n\n\t -- under construction --",
         )
-        .arg(
-            Arg::new("fasta")
-                .short('f')
-                .long("fasta")
-                .takes_value(false)
-                .help("Use fasta format records instead of lines"),
-        )
+        //.arg(
+        //    Arg::new("fasta")
+        //        .short('f')
+        //        .long("fasta")
+        //        .takes_value(false)
+         //       .help("Use fasta format records instead of lines"),
+        //)
         .arg(
             Arg::new("needle")
                 .short('p')
                 .long("--pattern")
-                .help("PATTERN to look for in FILE, e.g. a nucleotide sequence like 'AaTGATAcGGCGg'")
+                .help(
+                    "PATTERN to look for in FILE, e.g. a nucleotide sequence like 'AaTGATAcGGCGg'",
+                )
                 .required(true)
                 .takes_value(true)
                 .value_name("PATTERN"),
@@ -70,6 +118,7 @@ pub fn parse_args() -> NucGrepResult<Config> {
                 .takes_value(true)
                 .required(false)
                 .value_name("N")
+                .default_value("0")
                 .help("Maximum number of allowed non matching characters"),
         )
         .arg(
@@ -79,19 +128,218 @@ pub fn parse_args() -> NucGrepResult<Config> {
                 .help("Show only matches for the reverse complement of PATTERN")
                 .takes_value(false),
         )
+        .arg(
+            Arg::new("ignore_case")
+                .long("--ignore-case")
+                .short('i')
+                .help("Ignore case\nE.g. find match 'aTgA' in FILE for PATTERN 'ATGA' or 'atga' and vice versa"),
+        )
+        .arg(Arg::new("headers_only").long("--headers-only").short('H').help("Only show headers for records that match").takes_value(false))
         .get_matches();
     Ok(Config {
-        fasta: matches.is_present("fasta"),
+        fasta: true, //matches.is_present("fasta"),
         needle: matches.value_of("needle").map(String::from).unwrap(),
         file: matches.value_of_lossy("path").map(String::from).unwrap(),
         reverse_complement: matches.is_present("reverse_complement"),
+        only_reverse_complement: matches.is_present("only_reverse_complement"),
+        allow_non_matching: matches
+            .value_of("allow_non_matching")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(|e| {
+                eprintln!(
+                    "{}\n{}",
+                    e, "\nHint: --allow-non-matching takes a number as argument"
+                );
+                std::process::exit(2)
+            })
+            .unwrap(),
+        ignore_case: matches.is_present("ignore_case"),
+        headers_only: matches.is_present("headers_only"),
     })
 }
 
-pub fn open(path: String) -> NucGrepResult<Box<dyn BufRead>> {
+pub fn open(path: &String) -> NucGrepResult<Box<dyn BufRead>> {
     if path == "-" {
         Ok(Box::new(std::io::BufReader::new(std::io::stdin())))
     } else {
         Ok(Box::new(std::io::BufReader::new(File::open(path)?)))
+    }
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct NucleotideComplementError;
+
+impl Display for NucleotideComplementError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid nucleotide")
+    }
+}
+
+impl Error for NucleotideComplementError {}
+
+pub fn reverse_complement(
+    s: &str,
+    is_dna: Option<bool>,
+) -> Result<String, NucleotideComplementError> {
+    let is_dna_or_guess = match is_dna {
+        None => {
+            if s.find('u').is_some() || s.find('U').is_some() {
+                if s.find('t').is_some() || s.find('T').is_some() {
+                    return Err(NucleotideComplementError); // u and t in one sequence
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        }
+        Some(b) => b,
+    };
+    let complement = s
+        .chars()
+        .map(|c| match c {
+            'A' => {
+                if is_dna_or_guess {
+                    'T'
+                } else {
+                    'U'
+                }
+            }
+            'a' => {
+                if is_dna_or_guess {
+                    't'
+                } else {
+                    'u'
+                }
+            }
+            'G' => 'C',
+            'g' => 'c',
+            'T' => 'A',
+            't' => 'a',
+            'U' => 'A',
+            'u' => 'a',
+            'C' => 'G',
+            'c' => 'g',
+            'N' => 'N',
+            'n' => 'n',
+            'W' => 'W',
+            'w' => 'w',
+            'S' => 'S',
+            's' => 's',
+            'M' => 'K',
+            'm' => 'k',
+            'K' => 'M',
+            'k' => 'm',
+            'R' => 'Y',
+            'r' => 'y',
+            'Y' => 'R',
+            'y' => 'r',
+            'B' => 'V',
+            'b' => 'v',
+            'D' => 'H',
+            'd' => 'h',
+            'H' => 'D',
+            'h' => 'd',
+            'V' => 'B',
+            'v' => 'b',
+            '-' => '-',
+            _ => '?',
+        })
+        .collect::<String>();
+    if complement.contains('?') {
+        Err(NucleotideComplementError)
+    } else {
+        let rev: String = complement.chars().rev().collect();
+
+        Ok(rev)
+    }
+}
+
+mod tests {
+    use crate::{reverse_complement, NucleotideComplementError};
+
+    #[test]
+    pub fn test_revcomp() {
+        let input = "ATG";
+        let expected = "CAT";
+        assert_eq!(reverse_complement(&input, Some(true)).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_lower() {
+        let input = "atgcn";
+        let expected = "ngcat";
+        assert_eq!(reverse_complement(&input, Some(true)).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_lower_rna() {
+        let input = "augcn";
+        let expected = "ngcau";
+        assert_eq!(reverse_complement(&input, Some(false)).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_upper() {
+        let input = "GCTA";
+        let expected = "TAGC";
+        assert_eq!(reverse_complement(&input, Some(true)).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_upper_rna() {
+        let input = "GCUA";
+        let expected = "UAGC";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_mixed_rna() {
+        let input = "GcuA";
+        let expected = "UagC";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_guess() {
+        let input = "GGGGaaaaaaaatttatatat";
+        let expected = "atatataaattttttttCCCC";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_guess2() {
+        let input = "GGGGaaaaaaaauuuauauau";
+        let expected = "auauauaaauuuuuuuuCCCC";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_rna() {
+        let input = "AUG";
+        let expected = "CAU";
+        assert_eq!(reverse_complement(&input, Some(false)).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_lower_iupac() {
+        let input = "vnhdbatgcywsmkrd";
+        let expected = "hymkswrgcatvhdnb";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_upper_iupac() {
+        let input = "ATGCNHVBYWSMKRD";
+        let expected = "HYMKSWRVBDNGCAT";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_mixed_case_iupac() {
+        let input = "ATGCNhVBYWsMKRdD";
+        let expected = "HhYMKsWRVBdNGCAT";
+        assert_eq!(reverse_complement(&input, None).unwrap(), expected);
+    }
+    #[test]
+    pub fn test_revcomp_fail() {
+        let input = "blarghblergh!";
+        let expected = Err(NucleotideComplementError);
+        assert_eq!(reverse_complement(&input, None), expected);
+    }
+     #[test]
+    pub fn test_revcomp_fail_rna_dna() {
+        let input = "atgcu";
+        let expected = Err(NucleotideComplementError);
+        assert_eq!(reverse_complement(&input, None), expected);
     }
 }
---
+++ nucgrep/tests/cli.rs
@@ -0,0 +1,201 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use rand::{distributions::Alphanumeric, Rng};
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+const PRG: &str = "nucgrep";
+const EMPTY: &str = "tests/inputs/empty.txt";
+
+const FASTA_INVALID: &str = "tests/inputs/invalid.fa";
+
+const FASTA_ONE_RECORD_PER_LINE: &str = "tests/inputs/fasta_one_record_per_line.fa";
+const FASTA_WITH_LINEBREAKS: &str = "tests/inputs/fasta_with_line_breaks.fa";
+const FASTA_ONE_RECORD_PER_LINE_MC: &str = "tests/inputs/fasta_one_record_per_line_mc.fa"; //mixed case
+
+const FASTA_ONE_RECORD_PER_LINE_OUT: &str = "tests/inputs/fasta_one_record_per_line.fa.out";
+const FASTA_WITH_LINEBREAKS_OUT: &str = "tests/inputs/fasta_with_line_breaks.fa.out";
+const FASTA_ONE_RECORD_PER_LINE_MC_OUT: &str = "tests/inputs/fasta_one_record_per_line_mc.fa.out"; //mixed case
+
+const PATTERN_FOUND_ALL_UPPER: &str = "TACGGCCT";
+const PATTERN_FOUND_MC: &str = "TCGTgtgCAG";
+const PATTERN_FOUND_MC_ACROSS_LINEBREAK: &str = "TACGGCCT";
+
+// --------------------------------------------------
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin(PRG)?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+fn run_stdin(args: &[&str], input_file: &str, expected_file: &str) -> TestResult {
+    let input = fs::read_to_string(input_file)?;
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin(PRG)?
+        .write_stdin(input)
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+fn gen_bad_file() -> String {
+    loop {
+        let filename = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+// -- read file
+
+/* #[test]
+fn empty() -> TestResult {
+    run(&[EMPTY, "--pattern", "ATG"], "tests/expected/empty.txt.out")
+}*/
+//todo
+
+/*#[test]
+fn simple() -> TestResult {
+    run(&[EMPTY, "--pattern", "ATG"], "tests/expected/simple.fa.out")
+} */
+//todo
+
+/* #[test]
+fn simple_reverse_complement() -> TestResult {
+    run(
+        &[EMPTY, "--pattern", "CAT"],
+        "tests/expected/simple.fa.reverse_complement.out",
+    )
+} */
+//todo
+
+/*
+#[test]
+fn dies_fasta_invalid_file() -> TestResult {
+    let expected = format!("Error FASTA parse error: expected '>'");
+    Command::cargo_bin(PRG)?
+        .arg(FASTA_INVALID)
+        .arg("--pattern")
+        .arg("ATG")
+        .arg("--fasta")
+        .assert()
+        .failure()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}*/
+//todo
+
+#[test]
+fn dies_bad_file() -> TestResult {
+    let bad = gen_bad_file();
+    let expected = format!("{}: .* [(]os error 2[)]", bad);
+    Command::cargo_bin(PRG)?
+        .arg(bad)
+        .arg("--pattern")
+        .arg("ABC")
+        .assert()
+        .failure()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+
+// --- stdin ---
+#[test]
+fn dies_fasta_invalid_stdin() -> TestResult {
+    let input = fs::read_to_string(FASTA_INVALID)?;
+    let expected = "Error FASTA parse error: expected '>'";
+    Command::cargo_bin(PRG)?
+        .write_stdin(input)
+        .arg("--pattern")
+        .arg("A")
+        .assert()
+        .failure()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+
+#[test]
+fn dies_invalid_num_mismatch_args_stdin() -> TestResult {
+    let input = fs::read_to_string(FASTA_ONE_RECORD_PER_LINE)?;
+    let expected = format!("invalid digit found in string");
+    Command::cargo_bin(PRG)?
+        .write_stdin(input)
+        .arg("--pattern")
+        .arg("ATG")
+        .arg("--allow-non-matching")
+        .arg("A")
+        .assert()
+        .failure()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+
+#[test]
+fn empty_stdin() -> TestResult {
+    run_stdin(
+        &["--pattern", "ATG"],
+        "tests/inputs/empty.txt",
+        "tests/expected/empty.txt.out",
+    )
+}
+
+#[test]
+fn simple_stdin() -> TestResult {
+    run_stdin(
+        &["--pattern", "ATG"],
+        "tests/inputs/simple.fa",
+        "tests/expected/simple.fa.out",
+    )
+}
+#[test]
+fn simple_reverse_complement_stdin() -> TestResult {
+    run_stdin(
+        &["--pattern", "CAT", "--reverse-complement"],
+        "tests/inputs/simple.fa",
+        "tests/expected/simple.fa_reverse_complement.out",
+    )
+}
+#[test]
+fn simple_headers_only_stdin() -> TestResult {
+    run_stdin(
+        &["--pattern", "ATG", "--headers-only"],
+        "tests/inputs/simple.fa",
+        "tests/expected/simple.fa.headers_only.out",
+    )
+}
+#[test]
+fn simple_headers_only_reverse_complement_stdin() -> TestResult {
+    run_stdin(
+        &["--pattern", "CAT", "--headers-only", "--reverse-complement"],
+        "tests/inputs/simple.fa",
+        "tests/expected/simple.fa.headers_only_reverse_complement.out",
+    )
+}
+/* #[test]
+fn simple_reverse_complement() -> TestResult {
+    run(
+        &[EMPTY, "--pattern", "CAT"],
+        "tests/expected/simple.fa.reverse_complement.out",
+    )
+} */
+//todo
+
+// tests line break
+// test ignore case
+// test 1 mismatch
+// test 1 insertion
+// test 1 deletion
+// test multi mismatches/indels
+// test fail more mismatches than pattern length
